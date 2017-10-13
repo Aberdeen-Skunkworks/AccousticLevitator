6a8e0c68399765d53c2fdc3abf6922b5a1f4e2b3
NiallMacD <niall.macdonald5@gmail.com>
2017-10-13 23:14:44 +0100

    added a new file to write vti files and editied that function out of main program

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Created on Mon Sep 25 17:24:54 2017
 # -------------------------Import Libaries------------------------------------
 
 import constants; import numpy as np; from calc_pressure_field import calc_pressure_field
-import vtk; from array_grid import array_grid; from direction_vectors import direction_vectors
+from array_grid import array_grid; from direction_vectors import direction_vectors from vti_writer import vti_writer
 from phifind import phifind; from random_transducer_placment import random_transducer_placment
 
 
@@ -89,67 +89,7 @@ uy = uy + (p_mass*gravity) # including force of gravity
 
 # -------------------Creating output images and vtk file----------------------
 
-# creating vti image file with combined pressure magnitude data  
-
-filename = "writeImageData.vti"
-
-imageData = vtk.vtkImageData()
-imageData.SetDimensions(npoints, npoints, npoints )
-imageData.SetOrigin( (-npoints+1)/2, 0, (-npoints+1)/2 )
-if vtk.VTK_MAJOR_VERSION <= 5:
-    imageData.SetNumberOfScalarComponents(1)
-    imageData.SetScalarTypeToDouble()
-else:
-    imageData.AllocateScalars(vtk.VTK_DOUBLE, 1)
-
-dims = imageData.GetDimensions()
-
-# Fill every entry of the image data with "2.0"
-for z in range(dims[2]):
-    for y in range(dims[1]):
-        for x in range(dims[0]):
-            imageData.SetScalarComponentFromDouble(x, y, z, 0, realcombined[x,y,z])
-
-writer = vtk.vtkXMLImageDataWriter()
-writer.SetFileName(filename)
-if vtk.VTK_MAJOR_VERSION <= 5:
-    writer.SetInputConnection(imageData.GetProducerPort())
-else:
-    writer.SetInputData(imageData)
-writer.Write()
-
-# ----------------------------------------------------------------------------
-
-# creating vti image file with negitive gorkov potentials 
-
-filename2 = "Force.vti"
-
-imageDataForce = vtk.vtkImageData()
-imageDataForce.SetDimensions(npoints, npoints, npoints)
-imageDataForce.SetOrigin( (-npoints+1)/2, 0, (-npoints+1)/2 )
-if vtk.VTK_MAJOR_VERSION <= 5:
-    imageDataForce.SetNumberOfScalarComponents(3)
-    imageDataForce.SetScalarTypeToDouble()
-else:
-    imageDataForce.AllocateScalars(vtk.VTK_DOUBLE, 3)
-
-dims = imageDataForce.GetDimensions()
-
-# Fill every entry of the image data with "2.0"
-for z in range(dims[2]):
-    for y in range(dims[1]):
-        for x in range(dims[0]):
-            imageDataForce.SetScalarComponentFromDouble(x, y, z, 0, ux[x,y,z])
-            imageDataForce.SetScalarComponentFromDouble(x, y, z, 1, uy[x,y,z])
-            imageDataForce.SetScalarComponentFromDouble(x, y, z, 2, uz[x,y,z])
-
-writer = vtk.vtkXMLImageDataWriter()
-writer.SetFileName(filename2)
-if vtk.VTK_MAJOR_VERSION <= 5:
-    writer.SetInputConnection(imageDataForce.GetProducerPort())
-else:
-    writer.SetInputData(imageDataForce)
-writer.Write()
+vti_writer (npoints,realcombined,ux,uy,uz)
 
 
 print("Calculations compleated successfuly")
