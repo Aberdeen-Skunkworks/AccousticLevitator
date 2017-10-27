3b0524f075d4d8c9933e130374b60be4bc4397a7
NiallMacD <niall.macdonald5@gmail.com>
2017-10-27 21:48:25 +0100

    Finally got the analytical solution for the derivtive of the complex acoustic pressure

--- differentiation_algorithms.py
+++ differentiation_algorithms.py
@@ -39,12 +39,25 @@ def sin_theta (x, y, z, rt, phi, nt):
     sin_theta = math.sin(theta)
     return sin_theta
 
+def pressure (x, y, z, rt, phi, nt):
+
+    r = (x, y, z)
+    rs = np.subtract(r,rt)
+
+    mag_rs = np.linalg.norm(rs)
+
+    theta = math.acos((np.dot(rs,nt)) / (mag_rs))
+    exponential =  cmath.exp( 1j * (phi + k * mag_rs) ) / mag_rs
+    frac =  (constants.p0*constants.A)*( (math.sin(k*constants.a*math.sin(theta))) /  (k*constants.a*math.sin(theta)) )
+
+    pressure = (exponential*frac)
+    return pressure
 
 
 
 phi = math.pi   # Phase of the transducer
-x= 0.000011          # Point in space x,y,z
-y= 0.01001
+x= 0.00486648         # Point in space x,y,z
+y= 0.486601
 z= 0.06541
 h = 0.00000000001      # delta x the change to differientate over
 
@@ -101,9 +114,7 @@ print("Derivative with respect to z: Difference")
 print (abs(d_sin_theta_dr_numercal[2] - d_sin_theta_dr_analytical[2]))
 """
 
-
-
-
+"""
 ## Fraction term test
 
 df_dr_numercal_x_fraction =  (frac(x+h,y,z,rt,phi,nt) - frac(x-h,y,z,rt,phi,nt)) / (2*h)
@@ -113,7 +124,7 @@ df_dr_numercal_fraction = [df_dr_numercal_x_fraction, df_dr_numercal_y_fraction,
 # Analytical Terms
 numerator = constants.p0 * constants.A * math.sin(k*constants.a*math.sin(theta))
 denominator =  k*constants.a*math.sin(theta)
-d_denominator_dr = k*constants.a*(( ((-np.dot(rs,nt))/(mag_rs) )/ ((1 - (np.dot(rs,nt))/(mag_rs) )**0.5) ) * (nt*( ((1)/(mag_rs)) - ((2*rs_hat)/(mag_rs)) )))
+d_denominator_dr = k*constants.a*(( ((-np.dot(rs,nt))/(mag_rs) ) / ((1 - (np.dot(rs,nt))/(mag_rs) )**0.5) ) * ( (np.dot(nt,mag_rs) - (rs_hat*np.dot(nt,rs)) ) / (mag_rs**2) ) )
 d_numerator_dr = constants.p0 * constants.A * math.cos(k*constants.a*math.sin(theta))*d_denominator_dr
 
 d_fraction_dr_analytical = ((d_numerator_dr*denominator) - (numerator*d_denominator_dr))/(denominator**2)
@@ -128,4 +139,42 @@ print(d_fraction_dr_analytical[1])
 print("Derivative with respect to z: numerical then analytical")
 print (df_dr_numercal_fraction[2])
 print(d_fraction_dr_analytical[2])
+"""
+
+
+
+## Final Pressure derivation test
+
+dp_dr_numercal_x =  (pressure(x+h,y,z,rt,phi,nt) - pressure(x-h,y,z,rt,phi,nt)) / (2*h)
+dp_dr_numercal_y =  (pressure(x,y+h,z,rt,phi,nt) - pressure(x,y-h,z,rt,phi,nt)) / (2*h)
+dp_dr_numercal_z =  (pressure(x,y,z+h,rt,phi,nt) - pressure(x,y,z-h,rt,phi,nt)) / (2*h)
+dp_dr_numercal = [dp_dr_numercal_x, dp_dr_numercal_y, dp_dr_numercal_z]
+
+
+
+theta = math.acos((np.dot(rs,nt)) / (mag_rs))
+exponent_term =  cmath.exp( 1j * (phi + k * mag_rs) ) / mag_rs
+fraction_term =  (constants.p0*constants.A)*( (math.sin(k*constants.a*math.sin(theta))) /  (k*constants.a*math.sin(theta)) )
+d_exponential_term_dr = ((rs_hat * 1j * k * cmath.exp(1j*(phi + k * mag_rs)) ) / (mag_rs))  -  ((cmath.exp(1j*(phi + k * mag_rs)) * rs_hat) / (mag_rs**2) )
+numerator = constants.p0 * constants.A * math.sin(k*constants.a*math.sin(theta))
+denominator =  k*constants.a*math.sin(theta)
+d_denominator_dr = k*constants.a*(( ((-np.dot(rs,nt))/(mag_rs) ) / ((1 - (np.dot(rs,nt))/(mag_rs) )**0.5) ) * ( (np.dot(nt,mag_rs) - (rs_hat*np.dot(nt,rs)) ) / (mag_rs**2) ) )
+d_numerator_dr = constants.p0 * constants.A * math.cos(k*constants.a*math.sin(theta))*d_denominator_dr
+d_fraction_dr = ((d_numerator_dr*denominator) - (numerator*d_denominator_dr))/(denominator**2)
+
+
+d_p_dr = (d_fraction_dr * exponent_term) + (fraction_term * d_exponential_term_dr)
+
+
+
+print("Derivative with respect to x: Difference")
+print (abs(dp_dr_numercal[0] - d_p_dr[0]))
+print()
+print("Derivative with respect to y: Difference")
+print (abs(dp_dr_numercal[1] - d_p_dr[1]))
+print()
+print("Derivative with respect to z: Difference")
+print (abs(dp_dr_numercal[2] - d_p_dr[2]))
+
+
 
