e1de887d6aa20540e0291b18a2ce5c33fdde73c1
NiallMacD <niall.macdonald5@gmail.com>
2017-11-17 21:18:56 +0000

    Fixed bug where deletions cant be out of order

--- code_testing.py
+++ code_testing.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 import math
 
 
-rt = hex_grid(0.01,5,7)
-trans_to_delete = [0,1,31]                              # List of unwanted transducers
+rt = hex_grid(0.011,5,5)
+trans_to_delete = [0,1,4,5,18,16,24]                   # List of unwanted transducers 
 
-
+trans_to_delete = sorted(trans_to_delete)
 ntrans = len (rt)
 n_trans_to_delete = len(trans_to_delete)
 
@@ -22,7 +22,8 @@ plt.plot(rt[:,0,0], rt[:,0,2], 'ro')                    # Plots small dots
 
 for loop in range (0, (n_trans_to_delete)):             # Deletes unwanted transducers
     rt = np.delete(rt, trans_to_delete[loop], 0)
-
+    trans_to_delete = np.subtract(trans_to_delete, 1)
+    
 for trans in range (0, (ntrans - n_trans_to_delete)):   # Plots circles
     circle = plt.Circle((rt[trans,0,0], rt[trans,0,2]), 0.005, color='r')
     ax.add_artist(circle)
