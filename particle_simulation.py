82f3677a977aa7a667155dfdf1f0c5c1c0968b77
NiallMacD <niall.macdonald5@gmail.com>
2018-03-15 19:16:51 +0000

    Added bottle and vortex signature

--- particle_simulation.py
+++ particle_simulation.py
@@ -1,13 +1,13 @@
 import matplotlib as mpl; import numpy as np; import matplotlib.pyplot as plt; from mpl_toolkits.mplot3d import Axes3D;
 import math; import algorithms; import transducer_placment; import phase_algorithms; import constants; import time
  
-inital_pos = [0.001,0.001,0.0181]           # m
+inital_pos = [0.001,0.001,0.021]           # m
 inital_vel = [0,0,0]                        # m/s
 gravity    = [0, 0, -9.81]                  # m/s^2
 diamiter   = constants.particle_diamiter    # m    
 density    = constants.rhos                 # kg/m^3
 dt         = 0.0001                          # Time step s
-end_time   = 0.2                              # End time s
+end_time   = 0.5                              # End time s
 
 time_steps = end_time/dt
 print("Time steps = ", time_steps)
@@ -17,8 +17,8 @@ vol_sph =   constants.v                     # m^3
 mass    = vol_sph * density                 # kg 
 
 
-focus_point = [ 0 , 0, 0.018]
-
+focus_point = [ 0 , 0, 0.02]
+"""
 ##opposite arrays
 focus_point = [ 0 , 0, 0.05]         # in the middle of the arrays
 inital_pos = [0.001,0.001,0.051]
@@ -41,11 +41,11 @@ nt_both_arrays = np.append(nt_1, nt_2, axis=0)
 #transducer_placment.plot_as_vectors(rt_both_arrays,nt_both_arrays)  # Use to plot the array layout in 3D
 rt = rt_both_arrays
 nt = nt_both_arrays
+"""
 
-
-#rt = transducer_placment.big_daddy()
+rt = transducer_placment.big_daddy()
 ntrans = len (rt)
-#nt = transducer_placment.direction_vectors(ntrans,[0,0,1])
+nt = transducer_placment.direction_vectors(ntrans,[0,0,1])
 phi_focus = phase_algorithms.phase_find(rt, focus_point[0], focus_point[1], focus_point[2]) # phi is the initial phase of each transducer to focus on a point
 phi = phase_algorithms.add_twin_signature(rt,phi_focus, 90)
 
@@ -93,7 +93,7 @@ for time_step in range(0, number_of_time_steps):
     
     pos = np.add( pos, np.multiply(dt, vel), (acceleration*( (dt**2) / 2 ) ) )
   
-    force = np.add( force_g, np.multiply(-1, algorithms.differentiate_acoustic_potential(dt,pos,rt,phi,nt)))
+    force = np.add( force_g, np.multiply(-1, algorithms.differentiate_acoustic_potential(0.00000001,pos,rt,phi,nt)))
 
     acceleration_next = np.divide(force,mass)
     
@@ -102,8 +102,8 @@ for time_step in range(0, number_of_time_steps):
     acceleration = acceleration_next
     
     #total_energy[time_step] = algorithms.acoustic_potential(pos, rt, phi, nt) + (0.5 * mass * np.linalg.norm(vel)**2) + (mass * 9.81 * pos[2])
-    #if pos[2]<0:
-    #    break
+    if pos[2]<0:
+        break
 t2 = time.time()
 print("Time Taken = ", round(t2-t1,2)," s")
 
