cd0c53149134cc7d732bbc8e9f8eec1999965ae5
NiallMacD <niall.macdonald5@gmail.com>
2018-03-22 22:41:25 +0000

    Working flood fill hiariaricy in 1Dgit statusgit statusgit status

--- fill_algorithms.py
+++ fill_algorithms.py
@@ -175,12 +175,18 @@ class Tree(object):
     def add_child(self, node):
         assert isinstance(node, Tree)
         self.children.append(node)
+       
         
+def find_highest_parent(idx):
+    while regions_list[idx][0] != None:
+        idx = regions_list[idx][0]
+    return idx
 
 
 
 test_1d_array = [1,2,3,4,5,4,3,4,5,6,5,4,3,2,1,2,3,2,1,0]
-regions = [0] * len(test_1d_array)
+regions = [-1] * len(test_1d_array)
+regions_list = []
 
 dim = (len(test_1d_array))
 sorted_indices = []
@@ -188,23 +194,52 @@ sorted_indices = []
 sorted_indices = list(range(len(test_1d_array)))
 sorted_indices.sort(key=lambda idx : test_1d_array[idx])
 
-region = 1
+region = 0 # Starts at zero
 
 for pixle in range(dim):
     neighbours = nbhood(sorted_indices[pixle],[dim])
     assigned = [False] * len(neighbours)
-    print(neighbours)
+
     for num_nb in range(len(neighbours)):
-        if regions[neighbours[num_nb]] != 0:
+        if regions[neighbours[num_nb]] != -1:
             assigned[num_nb] = True
-    print(assigned)
-    if not any(assigned):
+    
+    if not any(assigned): # Check if there is zero assigned neighbours
         regions[sorted_indices[pixle]] = region
+        regions_list.append([None, []])
         region += 1
-    else:
-        pass
+        
+    elif sum(map(bool, assigned)) == 1: # Checks if there is only 1 assigned neighbour
+        for num_nb in range(len(neighbours)):
+            if regions[neighbours[num_nb]] != -1:
+                set_to_this_region = find_highest_parent(regions[neighbours[num_nb]])
+                regions[sorted_indices[pixle]] = set_to_this_region
+    
+    else: # multiple assigned neighbours
+        adj_regions = []
+        for num_nb in range(len(neighbours)):
+            adj_regions.append(regions[neighbours[num_nb]])
+        region_parents = list(map(find_highest_parent, adj_regions))
+        unique_region_parents = list(set(region_parents))
+        if len(unique_region_parents) == 1:
+            regions[sorted_indices[pixle]] = unique_region_parents[0]
+        else:
+            regions_list.append([None, unique_region_parents])
+            regions[sorted_indices[pixle]] = region
+            for len_region_parents in range(len(unique_region_parents)):
+                regions_list[unique_region_parents[len_region_parents]][0] = region
+            region += 1
+        
     
 
+
+
+
+
+
+
+
+"""
 from anytree import Node, RenderTree
 number = 1
 
@@ -218,7 +253,7 @@ print(region)
 
 from anytree.exporter import DotExporter
 DotExporter(region).to_dotfile("region.dot")
-
+"""
 
 
 
