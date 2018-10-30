3c528e6c469811c0fc36d51c426ee67384c19841
Marcus Bannerman <m.bannerman@gmail.com>
2018-10-30 11:07:21 +0000

    More work

--- UltrasonicLevitator_demo.py
+++ UltrasonicLevitator_demo.py
@@ -15,7 +15,8 @@ z = np.zeros((len(xs), len(ys)))
 def calcFields(shift:float =0):
     for i,x in enumerate(xs):
         for j,y in enumerate(ys):
-            z[i,j] = sys.pressure(Vector([x,y,0]), shift).real
+            #z[i,j] = sys.pressure(Vector([x,y,0]), shift).real
+            z[i,j] = sys.Gorkov_potential(Particle(Vector([x,y,0]), 7.176591426e-7, 0.0042))
 
 calcFields(0)
 
@@ -32,18 +33,41 @@ def animate(ishift):
     im.set_array(numpy.flip(z.T, 0))
     return im,
 
-import matplotlib.animation as animation
-anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
-anim.save('single_transducer.gif', fps=30, dpi=80, writer='imagemagick')
-sys.clear()
-for x in np.arange(-0.03, 0.035, 0.01):    
-    sys.appendTransducer(Vector([x,0,0]), Vector([0,1,0]))
-    rect = patches.Rectangle((x-0.005, -0.005), 0.01, 0.01, fill=True, facecolor="black", edgecolor="white")
-    ax.add_patch(rect)
+render_animations=False
 
-anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
-anim.save('multi_transducer.gif', fps=30, dpi=80, writer='imagemagick')
+if render_animations:
+    import matplotlib.animation as animation
+    anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
+    anim.save('single_transducer.gif', fps=30, dpi=80, writer='imagemagick')
+    sys.clear()
+    for x in np.arange(-0.03, 0.035, 0.01):    
+        sys.appendTransducer(Vector([x,0,0]), Vector([0,1,0]))
+        rect = patches.Rectangle((x-0.005, -0.005), 0.01, 0.01, fill=True, facecolor="black", edgecolor="white")
+        ax.add_patch(rect)
+        
+    anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
+    anim.save('multi_transducer.gif', fps=30, dpi=80, writer='imagemagick')
 
-sys.focus(Vector([0, 0.05, 0]))
-anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
-anim.save('multi_transducer_focussed.gif', fps=30, dpi=80, writer='imagemagick')
+    sys.focus(Vector([0, 0.05, 0]))
+    anim = animation.FuncAnimation(fig, animate, frames=30, interval=20, blit=True)
+    anim.save('multi_transducer_focussed.gif', fps=30, dpi=80, writer='imagemagick')
+else:
+    animate(0)
+    plt.show(block=False)
+    wait = input("PRESS ENTER TO CONTINUE.")
+    
+    sys.clear()
+    for x in np.arange(-0.03, 0.035, 0.01):    
+        sys.appendTransducer(Vector([x,0,0]), Vector([0,1,0]))
+        rect = patches.Rectangle((x-0.005, -0.005), 0.01, 0.01, fill=True, facecolor="black", edgecolor="white")
+        ax.add_patch(rect)
+
+    animate(0)
+    plt.draw()
+    wait = input("PRESS ENTER TO CONTINUE.")
+
+    sys.focus(Vector([0, 0.05, 0]))
+    animate(0)
+    print("Showing multi-focussed")
+    plt.draw()
+    wait = input("PRESS ENTER TO CONTINUE.")
